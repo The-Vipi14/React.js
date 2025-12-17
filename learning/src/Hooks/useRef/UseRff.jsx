17178dd36799e461d41a2923d1e3a3a4670bfc84
Vipin <vipinbhardwal286@gmail.com>
2025-12-18 01:47:52 +0530

    react Hooks updated: useRef, useState

--- learning/src/Hooks/useRef/UseRff.jsx
+++ learning/src/Hooks/useRef/UseRff.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import { useRef, useState } from "react";
+import "./style.css";
+export const UseRff = () => {
+  const username = useRef("");
 
-const UseRff = () => {
+  const focusUsername = () => {
+    username.current.focus();
+  };
+
   return (
-    <div>UseRff</div>
-  )
-}
+    <>
+      <h1>basic use of useRef : select dom element: focus</h1>
+      <input type="text" ref={username} />
+      <button onClick={focusUsername}>focus</button>
+    </>
+  );
+};
 
-export default UseRff
+export const FormHandling = () => {
+  const usernameRef = useRef(null);
+  const passwordRef = useRef(null);
+
+  const [error, seterror] = useState("");
+  const submitForm = (e) => {
+    e.preventDefault();
+
+    if (usernameRef.current.value === "") {
+      seterror("Username is required");
+      usernameRef.current.focus();
+      return;
+    }
+    if (passwordRef.current.value === "") {
+      seterror("Username is required");
+      passwordRef.current.focus();
+      return;
+    }
+    const userDetails = {
+      username: usernameRef.current.value,
+      password: passwordRef.current.value,
+    };
+
+    console.log(userDetails);
+
+    seterror("");
+    usernameRef.current.value = "";
+    passwordRef.current.value = "";
+  };
+  return (
+    <>
+      <h2>Form handing with useRef Hook</h2>
+      <form onSubmit={submitForm}>
+        <label htmlFor="username">username</label>
+        <input type="text" id="username" ref={usernameRef} />
+        <label htmlFor="password">password</label>
+        <input type="text" id="password" ref={passwordRef} />
+        {error && <p style={{ color: "red" }}>{error}</p>}
+        <button type="submit">submit</button>
+      </form>
+    </>
+  );
+};
---
+++ learning/src/Hooks/useState/UseState.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import "./style.css";
+
+const UseState = () => {
+  const [username, setusername] = useState("");
+  const [password, setpassword] = useState("");
+
+  const handleForm = (e) => {
+    e.preventDefault();
+
+    const userDetails = {
+      username: username,
+      password: password,
+    };
+
+    console.log(userDetails);
+
+    setusername("")
+    setpassword("")
+  };
+
+  return (
+    <>
+      <h1>Form handling by useState </h1>
+      <form onSubmit={handleForm}>
+        <label htmlFor="username">username</label>
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setusername(e.target.value)}
+        />
+        <label htmlFor="password">password</label>
+        <input
+          type="text"
+          value={password}
+          onChange={(e) => setpassword(e.target.value)}
+        />
+
+        <button type="submit">submit</button>
+      </form>
+    </>
+  );
+};
+
+export default UseState;
+ 
