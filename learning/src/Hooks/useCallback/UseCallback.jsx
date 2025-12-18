3e90b7e72cbcc58e6c456b311d4a356aad0041fe
Vipin <vipinbhardwal286@gmail.com>
2025-12-18 23:30:48 +0530

    useCallback concept.txt

--- learning/src/Hooks/useCallback/UseCallback.jsx
+++ learning/src/Hooks/useCallback/UseCallback.jsx
@@ -1,15 +1,18 @@
-import {useState, useCallback}from 'react'
+import { useState, useCallback } from "react";
 
 export const UseCallback = () => {
-    const [count, setcount] = useState(0);
-    const increment = useCallback(()=>{
-        setcount(pre => pre + 1);
-    },[]);
+  const [count, setcount] = useState(0);
+  const increment = useCallback(() => {
+    setcount((pre) => pre + 1);
+  }, []);
+  const decrement = useCallback(() => {
+    setcount(count - 1);
+  }, [count]);
   return (
     <>
-    <h2>{count}</h2>
-    <button onClick={increment}>+</button>
+      <h2>{count}</h2>
+      <button onClick={increment}>+</button>
+      <button onClick={decrement}>-</button>
     </>
-  )
-}
-
+  );
+};
