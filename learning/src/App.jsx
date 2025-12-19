f4f8893eed9ff08a86a379e55427b032bc6b3f5c
Vipin <vipinbhardwal286@gmail.com>
2025-12-19 12:03:39 +0530

    Small task - change theme using toggle state and conditional styling

--- learning/src/App.jsx
+++ learning/src/App.jsx
@@ -1,15 +1,23 @@
-import Navbar from './components/Navbar/Navbar';
-import Concept from './Hooks/useEffect/Concept';
-import Page1 from './pages/Page1';
-
+import Navbar from "./components/Navbar/Navbar";
+import Concept from "./Hooks/useEffect/Concept";
+import { UseRff, FormHandling ,ScrollExample} from "./Hooks/useRef/UseRff";
+import Page1 from "./pages/Page1";
+import UseState from "./Hooks/useState/useState";
+import { UseCallback } from "./Hooks/useCallback/useCallback";
+import Theme from "./SmallTasks/ThemePage/Theme";
 const App = () => {
   return (
     <>
-    <Navbar/>
-    {/* <Concept/> */}
-    <Page1/>
+      <Navbar />
+      {/* <Concept/> */}
+      {/* <Page1/> */}
+      {/* <ScrollExample/> */}
+      {/* <FormHandling /> */}
+      {/* <UseState /> */}
+      {/* <UseCallback/> */}
+      <Theme/>
     </>
-  )
-}
+  );
+};
 
 export default App;
---
+++ learning/src/SmallTasks/ThemePage/Theme.jsx
@@ -0,0 +1,15 @@
+import { useEffect, useState } from "react";
+import "./style.css";
+const Theme = () => {
+  const [theme, setTheme] = useState(false)
+  return (
+    <>
+      <div className={!theme ? "box" : "box box-dark"}>
+        <h1>Theme page</h1>
+        <button onClick={()=>!theme ? setTheme(true) : setTheme(false)}>change theme</button>
+      </div>
+    </>
+  );
+};
+
+export default Theme;
