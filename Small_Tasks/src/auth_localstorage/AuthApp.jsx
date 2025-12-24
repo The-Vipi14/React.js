4c13433f22169f9886b403ae420d94283ba5022b
Vipin <vipinbhardwal286@gmail.com>
2025-12-24 21:34:55 +0530

    login logic started

--- Small_Tasks/src/auth_localstorage/AuthApp.jsx
+++ Small_Tasks/src/auth_localstorage/AuthApp.jsx
@@ -23,6 +23,7 @@ const AuthApp = () => {
   // useEffect(() => {
   //   console.log("database updated:", database); // printing value
   // }, [database]);
+
   return (
     <>
       <BrowserRouter>
@@ -39,35 +40,3 @@ const AuthApp = () => {
 };
 
 export default AuthApp;
-
-// import { useContext, useEffect, useState } from "react";
-// import userContext from "./context/userContext";
-// import Home from "./pages/Home";
-// import Login from "./pages/Login";
-// import Signup from "./pages/Signup";
-
-// const AuthApp = () => {
-//   const [loggedInuser, setLoggedInuser] = useState({});
-//   const [isLoggedIn, setIsLoggedIn] = useState(false);
-//   const [database, setdatabase] = useState([]);
-
-//   useEffect(() => {
-//     const data = JSON.parse(localStorage.getItem("dataBase")) || [];
-//     setdatabase(data);
-
-//     const loginStatus = JSON.parse(localStorage.getItem("isLoggedIn")) || false;
-//     setIsLoggedIn(loginStatus);
-//   }, []);
-
-//   return (
-//     <userContext.Provider
-//       value={{ isLoggedIn, loggedInuser, database, setdatabase }}
-//     >
-//       <Home />
-//       <Login />
-//       <Signup />
-//     </userContext.Provider>
-//   );
-// };
-
-// export default AuthApp;
--- Small_Tasks/src/auth_localstorage/pages/Login.jsx
+++ Small_Tasks/src/auth_localstorage/pages/Login.jsx
@@ -1,16 +1,48 @@
-import { useContext } from "react";
+import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
+
+import { useContext, useState } from "react";
 import userContext from "../context/userContext";
 
 const Login = () => {
-  const { isLoggedIn, dataBase } = useContext(userContext);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { isLoggedIn, database } = useContext(userContext);
+
   const show = () => {
-    console.log(dataBase);
+    console.log(database);
+  };
+  const login = (e) => {
+    e.preventDefault();
   };
 
   return (
-    <div>
-      <button onClick={show}>show data</button>
-    </div>
+    <>
+      <div>
+        <h1>Login </h1>
+        <form onSubmit={login}>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            placeholder="Email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="password"
+            id="password"
+          />
+          <button type="submit">submit</button>
+          <Link to={"/signup"}>create an accout.</Link>
+        </form>
+      </div>
+    </>
   );
 };
 
